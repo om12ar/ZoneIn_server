47cb74c9a309206880ff8653ece4937e4d247b12
Maha Mohamed <Maha Mohamed>
2016-04-28 01:34:43 +0200

    Fixing Database Connection

--- src/com/models/DBConnection.java
+++ src/com/models/DBConnection.java
@@ -17,13 +17,13 @@ public class DBConnection {
 		/*	connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ZoneIn_DB?"
 					+ "user=root&password=&characterEncoding=utf8");
 			*/
-//			connection = DriverManager
-//					.getConnection("jdbc:mysql://127.6.228.2:3306/zonein?"
-//							+ "user=admin2sLmsaB&password=f-ThUdYmsN34&characterEncoding=utf8");
+			connection = DriverManager
+					.getConnection("jdbc:mysql://127.6.228.2:3306/zonein?"
+							+ "user=admin2sLmsaB&password=f-ThUdYmsN34&characterEncoding=utf8");
 
 
-			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/zonein?"
-					+ "user=root&password=root&characterEncoding=utf8");
+//			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/zonein?"
+//					+ "user=root&password=root&characterEncoding=utf8");
 							
 
 			return connection;
